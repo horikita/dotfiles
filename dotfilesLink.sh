c2d2e0d8c90c9460493b527f113127a397436fba
kou5643 <kou5643@gmail.com>
2021-09-25 18:42:05 +0900

    fix filename and process

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,7 @@
- ~/dotfiles/.vimrc ~/.vimrc
- ln -s ~/dotfiles/.gvimrc ~/.gvimrc
- ln -s ~/dotfiles/.bashrc ~/.bashrc
- ln -s ~/dotfiles/.bash_profile ~/.bash_profile
- ln -s ~/dotfiles/.vim ~/.vim
- ln -s ~/dotfiles/.gitconfig ~/.gitconfig
- ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/git/dotfiles ~/dotfiles 
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
 
